9fef2481f896276590982eff5807c383c1a3b5b7
Red <vhanchar@ucsc.edu>
2019-07-17 12:29:32 -0700

    added nav bar

---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const NavBar = () => {
+  return (
+    <div className='navBar'> 
+    My navbar to be changed later.
+    </div>
+  );
+};
+
+export default NavBar;
