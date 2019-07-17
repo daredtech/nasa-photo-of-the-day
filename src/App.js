f994c6db457209cbe007c3f8cd67786aa5ede789
Red <vhanchar@ucsc.edu>
2019-07-17 12:36:43 -0700

    added nasa card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
+import NasaCard from "./NasaCard";
 import "./App.css";
 
 function App() {
@@ -7,9 +8,22 @@ function App() {
     <div className="App">
      
      <NavBar />
+     <NasaCard />
      
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// In App.js (or where ever you wanted to fetch the data) add state for the data you'll be getting from NASA.
+// Add an effect hook to handle the API call side effect.
+// Go to the NASA APOD API docs and read through the docs to see how to make the API call.
+// You don't need an API key. However you may need one if you exceed the API request limits.
+// Using the endpoint given, fetch the data using axios.
+// In your .then() make sure to console.log the response so you can look at the shape of the data. 😃
+// Before you add your data to state, make sure your effect hook has a dependency array (probably empty, since we don't want this effect synced up to any state/props), otherwise you will start an infinite loop, and you will exceed the API rate limits of the DEMO_KEY and need to use a real API_KEY.
---
+++ src/NasaCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NasaCard = () => {
+    return(
+        <div className='nasa-card'>
+            My NasaCard to be changed later
+        </div>
+    );
+}
+
+export default NasaCard;
