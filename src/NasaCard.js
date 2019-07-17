f1d84aefdad8afcd95e5f8852697844c9aaadfb7
Red <vhanchar@ucsc.edu>
2019-07-17 12:57:32 -0700

    added hard-coded img

--- src/NasaCard.js
+++ src/NasaCard.js
@@ -1,9 +1,11 @@
 import React from "react"
+import NasaImg from "./NasaImg"
 
 const NasaCard = () => {
     return(
         <div className='nasa-card'>
             My NasaCard to be changed later
+            <NasaImg />
         </div>
     );
 }
---
+++ src/NasaImg.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NasaImg = () => {
+
+ 
+    return(
+        <div className = "nasa-img">
+        <img src='https://pbs.twimg.com/profile_images/1074223648/nasalogo_twitter_400x400.jpg'/>
+        </div>
+    );
+}
+
+export default NasaImg;
