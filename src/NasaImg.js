c55fed70882c36b0a1ffdf2eaaa4339ff53058ac
Red <vhanchar@ucsc.edu>
2019-07-18 14:06:55 -0700

    added fluid from semantic-ui.

--- src/NasaImg.js
+++ src/NasaImg.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Iframe from 'react-iframe'
 import NasaCard from "./NasaCard"
+import { Image } from 'semantic-ui-react'
 
 const NasaImg = (props) => {
     const {imgValue} = props;
+    console.log("I am a testing value in NasaImg", imgValue);
+
     return(
         <div className = "nasa-img">
-        <img src={imgValue} alt='nasa-img'/>
+        <Image src={imgValue}  fluid />
        
         {/* for videos: */}
         {/* <Iframe src={testing} alt='nasa-video'/>   */}
